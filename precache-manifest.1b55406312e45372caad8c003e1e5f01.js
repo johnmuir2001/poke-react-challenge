7ea1bc19d806a3e6f787484af68c77bfdf111a0e
John Muir <johnmuir1907@gmail.com>
2020-02-26 14:16:26 +0000

    Updates

---
+++ precache-manifest.1b55406312e45372caad8c003e1e5f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66086b7c13d1d669d41880fc917878ea",
+    "url": "/poke-react-challenge/index.html"
+  },
+  {
+    "revision": "2b9c9b0481e1a3bda092",
+    "url": "/poke-react-challenge/static/css/main.64c2db68.chunk.css"
+  },
+  {
+    "revision": "ad14f3b81fb06fb532a3",
+    "url": "/poke-react-challenge/static/js/2.387cb5f6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/poke-react-challenge/static/js/2.387cb5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9c9b0481e1a3bda092",
+    "url": "/poke-react-challenge/static/js/main.8f8d6e32.chunk.js"
+  },
+  {
+    "revision": "d8a89f42a802ea7a10f5",
+    "url": "/poke-react-challenge/static/js/runtime-main.0e3f5f73.js"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "/poke-react-challenge/static/media/pokemon.49255cb2.png"
+  }
+]);
